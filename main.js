301960ebc494e054bea122ffe42061e58d26e67d
olivier <oliviersenn6@gmail.com>
2019-03-01 09:50:53 +0100

    first build working
    connect to the proxy before finishing the queue!

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
 //imports
 var mc = require('minecraft-protocol'); //duh
-var fs = require('fs');
+var fs = require('fs'); //to read creds file
+var webserver = require('./webserver.js'); //to serve the webserver
 
 
 
 var secrets = JSON.parse(fs.readFileSync('secrets.json'));
 
+
+webserver.createServer(80);
+
+var proxyClient;
 
 var client = mc.createClient({
   host: "2b2t.org",
@@ -16,13 +21,63 @@ var client = mc.createClient({
   version: "1.12.2"
 });
 
+webserver.username = client.username;
+
 client.on("packet", function(data,meta){
     if(meta.name === "playerlist_header"){
         var headermessage = JSON.parse(data.header);
         var positioninqueue = headermessage.text.split("\n")[5].substring(25);
         var ETA = headermessage.text.split("\n")[6].substring(27);
-        console.log("Position: " +positioninqueue + "\nETA: " + ETA);
-        
+        webserver.queuePlace = positioninqueue;
+        webserver.ETA = ETA;
+    }
+    if (proxyClient) {
+        filterPacketAndSend(data, meta, proxyClient);
     }
     // console.log("packet  meta: " + JSON.stringify(meta) +"\n\tdata: "+JSON.stringify(data));
 });
+
+
+
+//client.on raw
+
+var server = mc.createServer({
+    'online-mode':false,
+    encryption:true,
+    host:'0.0.0.0',
+    port:25565,
+    version:'1.12.2',
+    maxPlayers: 1
+})
+
+server.on('login', function(newProxyClient){
+    newProxyClient.write('login', {
+        entityId: newProxyClient.id,
+        levelType: 'default',
+        gameMode: 0,
+        dimension: 0,
+        difficulty: 2,
+        maxPlayers: server.maxPlayers,
+        reducedDebugInfo: false
+      });
+    newProxyClient.write('position', {
+        x: 0,
+        y: 1.62,
+        z: 0,
+        yaw: 0,
+        pitch: 0,
+        flags: 0x00
+      });
+
+      newProxyClient.on('packet', function(data, meta){                    
+          filterPacketAndSend(data, meta, client);
+      })
+
+    proxyClient = newProxyClient;
+});
+
+function filterPacketAndSend(data, meta, dest) {
+    if (meta.name !="keep_alive" && meta.name !="update_time") {
+        dest.write(meta.name, data);
+    }
+}
