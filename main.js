0e0127888781b0710e559db0e7eff5525ceb8c2f
GisliNielsen <Gislionielsen@gmail.com>
2019-05-12 19:00:49 +0200

    Neatified the code

--- main.js
+++ main.js
@@ -1,141 +1,129 @@
 
-//imports
-var mc = require('minecraft-protocol'); //duh
-var fs = require('fs'); //to read creds file
-var webserver = require('./webserver.js'); //to serve the webserver
+// imports
+const mc = require('minecraft-protocol'); // to handle minecraft login session
+const fs = require('fs'); // to read creds file
+const webserver = require('./webserver.js'); // to serve the webserver
 
+const secrets = JSON.parse(fs.readFileSync('secrets.json')); // read the creds
+const config = JSON.parse(fs.readFileSync('config.json')); // read the config
 
-
-var secrets = JSON.parse(fs.readFileSync('secrets.json')); //read the creds
-var config = JSON.parse(fs.readFileSync('config.json')); //read the config
-
-
-
-webserver.createServer(config.ports.web); //create the webserver
-webserver.onstart(function() { //set up actions for the webserver
-    startQueuing();
+webserver.createServer(config.ports.web); // create the webserver
+webserver.onstart(() => { // set up actions for the webserver
+	startQueuing();
 });
-webserver.onstop(function(){
-    stop();
-})
+webserver.onstop(() => {
+	stop();
+});
 
+// lets
+let proxyClient; // a reference to the client that is the actual minecraft game
+let client; // the client to connect to 2b2t
+let server; // the minecraft server to pass packets
 
-//vars
-var proxyClient; //a reference to the client that is the actual minecraft game
-var client; //the client to connect to 2b2t
-var server; //the minecraft server to pass packets
-
-
-//function to disconnect from the server
+// function to disconnect from the server
 function stop(){
-    webserver.isInQueue = false;
-    webserver.queuePlace = "None";
-    webserver.ETA = "None";
-    client.end(); //disconnect
-    if (proxyClient) {
-        proxyClient.end("Stopped the proxy."); //boot the player from the server
-    }
-    server.close(); //close the server
+	webserver.isInQueue = false;
+	webserver.queuePlace = "None";
+	webserver.ETA = "None";
+	client.end(); // disconnect
+	if (proxyClient) {
+		proxyClient.end("Stopped the proxy."); // boot the player from the server
+	}
+	server.close(); // close the server
 }
 
-
-//function to start the whole thing
+// function to start the whole thing
 function startQueuing() {
-    webserver.isInQueue = true;
-    client = mc.createClient({ //connect to 2b2t
-        host: "2b2t.org",
-        port: 25565,
-        username: secrets.username,
-        password: secrets.password,
-        version: "1.12.2"
-      });
-    var finishedQueue = false;
-    client.on("packet", function(data,meta){ //each time 2b2t sends a packet
-        if(!finishedQueue && meta.name === "playerlist_header"){ //if the packet contains the player list, we can use it to see our place in the queue
-            var headermessage = JSON.parse(data.header);
-            var positioninqueue = headermessage.text.split("\n")[5].substring(25);
-            var ETA = headermessage.text.split("\n")[6].substring(27);
-            webserver.queuePlace = positioninqueue; //update info on the web page
-            webserver.ETA = ETA;
-            server.motd = "Place in queue: " +  positioninqueue; //set the MOTD because why not
-        }
-        if (!finishedQueue && meta.name === "chat") { //we can know if we're about to finish the queue by reading the chat message
-            //we need to know if we finished the queue otherwise we crash when we're done, because the queue info is no longer in packets the server sends us.
-            var chatMessage = JSON.parse(data.message);
-            if(chatMessage.text && chatMessage.text === "Connecting to the server..."){
-                finishedQueue = true;
-                webserver.queuePlace = "FINISHED";
-                webserver.ETA = "NOW";
-            }
-        } 
-    
-        if (proxyClient) { //if we are connected to the proxy, forward the packet we recieved to our game.
-            filterPacketAndSend(data, meta, proxyClient);
-        }
-        // console.log("packet  meta: " + JSON.stringify(meta) +"\n\tdata: "+JSON.stringify(data));
-    });
-    
-    //set up actions in case we get disconnected.
-    client.on('end', function(){
-        if (proxyClient) {
-            proxyClient.end("Connection reset by 2b2t server.\nReconnecting...");
-        }
-        stop();
-        setTimeout(startQueuing, 100); //reconnect after 100 ms
-    });
+	webserver.isInQueue = true;
+	client = mc.createClient({ // connect to 2b2t
+		host: "2b2t.org",
+		port: 25565,
+		username: secrets.username,
+		password: secrets.password,
+		version: "1.12.2"
+	});
+	let finishedQueue = false;
+	client.on("packet", (data, meta) => { // each time 2b2t sends a packet
+		if (!finishedQueue && meta.name === "playerlist_header") { // if the packet contains the player list, we can use it to see our place in the queue
+			let headermessage = JSON.parse(data.header);
+			let positioninqueue = headermessage.text.split("\n")[5].substring(25);
+			let ETA = headermessage.text.split("\n")[6].substring(27);
+			webserver.queuePlace = positioninqueue; // update info on the web page
+			webserver.ETA = ETA;
+			server.motd = `Place in queue: ${positioninqueue}`; // set the MOTD because why not
+		}
+		if (finishedQueue === false && meta.name === "chat") { // we can know if we're about to finish the queue by reading the chat message
+			// we need to know if we finished the queue otherwise we crash when we're done, because the queue info is no longer in packets the server sends us.
+			let chatMessage = JSON.parse(data.message);
+			if (chatMessage.text && chatMessage.text === "Connecting to the server...") {
+				finishedQueue = true;
+				webserver.queuePlace = "FINISHED";
+				webserver.ETA = "NOW";
+			}
+		}
 
-    client.on('error', function(err){
-        if (proxyClient) {
-            proxyClient.end("Connection error by 2b2t server.\n Error message: " + err + "\nReconnecting...");
-        }        
-        stop();
-        setTimeout(startQueuing, 100); //reconnect after 100 ms
-    });
-    
-    
-    
-    server = mc.createServer({ //create a server for us to connect to
-        'online-mode':false,
-        encryption:true,
-        host:'0.0.0.0',
-        port:config.ports.minecraft,
-        version:'1.12.2',
-        maxPlayers: 1
-    })
-    
-    server.on('login', function(newProxyClient){ //handle login stuff
-        newProxyClient.write('login', {
-            entityId: newProxyClient.id,
-            levelType: 'default',
-            gameMode: 0,
-            dimension: 0,
-            difficulty: 2,
-            maxPlayers: server.maxPlayers,
-            reducedDebugInfo: false
-          });
-        newProxyClient.write('position', {
-            x: 0,
-            y: 1.62,
-            z: 0,
-            yaw: 0,
-            pitch: 0,
-            flags: 0x00
-          });
-    
-          newProxyClient.on('packet', function(data, meta){//redirect everything we do to 2b2t            
-              filterPacketAndSend(data, meta, client);
-          })
-    
-        proxyClient = newProxyClient;
-    });
+		if (proxyClient) { // if we are connected to the proxy, forward the packet we recieved to our game.
+			filterPacketAndSend(data, meta, proxyClient);
+		}
+	});
+
+	// set up actions in case we get disconnected.
+	client.on('end', () => {
+		if (proxyClient) {
+			proxyClient.end("Connection reset by 2b2t server.\nReconnecting...");
+		}
+		stop();
+		setTimeout(startQueuing, 100); // reconnect after 100 ms
+	});
+
+	client.on('error', (err) => {
+		if (proxyClient) {
+			proxyClient.end(`Connection error by 2b2t server.\n Error message: ${err}\nReconnecting...`);
+		}
+		stop();
+		setTimeout(startQueuing, 100); // reconnect after 100 ms
+	});
+
+	server = mc.createServer({ // create a server for us to connect to
+		'online-mode': false,
+		encryption: true,
+		host: '0.0.0.0',
+		port: config.ports.minecraft,
+		version: '1.12.2',
+		maxPlayers: 1
+	});
+
+	server.on('login', (newProxyClient) => { // handle login
+		newProxyClient.write('login', {
+			entityId: newProxyClient.id,
+			levelType: 'default',
+			gameMode: 0,
+			dimension: 0,
+			difficulty: 2,
+			maxPlayers: server.maxPlayers,
+			reducedDebugInfo: false
+		});
+		newProxyClient.write('position', {
+			x: 0,
+			y: 1.62,
+			z: 0,
+			yaw: 0,
+			pitch: 0,
+			flags: 0x00
+		});
+
+		newProxyClient.on('packet', (data, meta) => { // redirect everything we do to 2b2t
+			filterPacketAndSend(data, meta, client);
+		});
+
+		proxyClient = newProxyClient;
+	});
 }
-
-
 
 //function to filter out some packets that would make us disconnect otherwise.
 //this is where you could filter out packets with sign data to prevent chunk bans.
 function filterPacketAndSend(data, meta, dest) {
-    if (meta.name !="keep_alive" && meta.name !="update_time") { //keep alive packets are handled by the client we created, so if we were to forward them, the minecraft client would respond too and the server would kick us for responding twice.
-        dest.write(meta.name, data);
-    }
+	if (meta.name !="keep_alive" && meta.name !="update_time") { //keep alive packets are handled by the client we created, so if we were to forward them, the minecraft client would respond too and the server would kick us for responding twice.
+		dest.write(meta.name, data);
+	}
 }
--- webserver.js
+++ webserver.js
@@ -1,40 +1,42 @@
 //this module exposes functions and variables to control the HTTP server.
-var http = require('http'); //to serve the pages
-var fs = require('fs'); //to read the webpages from disk
+const http = require('http'); //to serve the pages
+const fs = require('fs'); //to read the webpages from disk
 
 module.exports = {
     createServer : function(port) {
-        http.createServer(function(req, res){
-            if (req.url == "/index.html" || req.url == "/") { //main page of the web app
+        http.createServer(function(req, res) {
+            if (req.url === "/") { //main page of the web app
                 res.writeHead(200, {'Content-type': 'text/html'});
                 res.write(fs.readFileSync('index.html'));
                 res.end();
-            }else if(req.url == "/index.css"){ //css file to make it not look like too much shit
+            } else if(req.url === "/index.css") { //css file to make it not look like too much shit
                 res.writeHead(200, {'Content-type': 'text/css'});
                 res.write(fs.readFileSync('index.css'));
                 res.end();
-            }else if(req.url == "/update"){ //API endpoint to get position, ETA, and status in JSON format      
+            } else if(req.url === "/update") { //API endpoint to get position, ETA, and status in JSON format      
                 res.writeHead(200, {'Content-type': 'text/json'});
                 res.write("{\"username\": \""+ module.exports.username +"\",\"place\": \""+ module.exports.queuePlace +"\",\"ETA\": \""+ module.exports.ETA +"\", \"inQueue\": " + module.exports.isInQueue+"}")
                 res.end();
-            }else if(req.url == "/start"){ //API endpoint to start queuing
+            } else if(req.url === "/start") { //API endpoint to start queuing
                 res.writeHead(200);
                 res.end();
                 module.exports.onstartcallback();
-            }else if(req.url == "/stop"){ //API endpoint to stop queuing
+            } else if(req.url === "/stop") { //API endpoint to stop queuing
                 res.writeHead(200);
                 res.end();
                 module.exports.onstopcallback();
-            }else{
+            } else {
                 res.writeHead(404);
                 res.end();
             }
         }).listen(port);
     },
-    onstart:function(callback){ //function to set the action to do when starting
+    onstart:function(callback) { //function to set the action to do when starting
+        console.log('Webserver is online');
         module.exports.onstartcallback = callback;
     },
     onstop:function(callback) { //same but to stop
+        console.log('Webserver is offline');
         module.exports.onstopcallback = callback;
     },
     queuePlace : "None", //our place in queue
