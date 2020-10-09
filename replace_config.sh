8e6fa4d12bf4132ca9eed93df844d370995834de
Nathan Bourdeau <nathan@infiniit.co>
2020-10-09 17:27:15 -0400

    check for config.json before attempting replacement
    replace_config.sh didn't check if config.json already existed, so it just clobbered it. copied the code from replace_credentials.sh to check for config.json before overwriting

--- replace_config.sh
+++ replace_config.sh
@@ -4,6 +4,10 @@
 
 # script used to set the config values in the docker image
 
+# if file already exists it means it has been mounted, I'll use that one
+
+if [ ! -f /srv/app/config.json ]; then
+
 # create config file
 cp config.json.example config.json
 
@@ -12,3 +16,5 @@ sed -i 's/DISCORDBOT_FLAG/'"$DISCORD_BOT"'/g' config.json
 sed -i 's/WEBSERVER_FLAG/'"$WEBSERVER"'/g' config.json
 sed -i 's/MINECRAFT_PROXY_PORT/'"$MINECRAFT_PORT"'/g' config.json
 sed -i 's/WEB_UI_PORT/'"$WEBUI_PORT"'/g' config.json
+
+fi
