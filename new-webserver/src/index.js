ef7f5651dd3c81012e4f2ccae1f04b8efafa78e9
Shravan Mandava <shravanmandava@outlook.com>
2021-04-14 18:58:52 +0100

    try and move to react

---
+++ new-webserver/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Particles />
+        )
+    }
+}
