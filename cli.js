d587d2dcd3bf8f155ab04b99529d5053c57b0d2b
MrGeorgen <gf2wb6f7t6tfg67wbf8ebgft6ef@protonmail.com>
2021-05-06 22:30:50 +0200

    fix issues on windows with the standalone binary

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const util = require("./util");
-let dir = `${process.env.HOME}/.2bored2wait`
+const os = require("os");
+const path = require("path");
+let dir = `${os.homedir()}/.2bored2wait`
 util.mkdir(`${dir}/config`);
 process.chdir(dir);
-process.env.NODE_CONFIG_DIR = `./config:./nexe-conf`;
-require("./main");
+process.env.NODE_CONFIG_DIR = `./config${path.delimiter}nexe-conf`;
+require("./check");
