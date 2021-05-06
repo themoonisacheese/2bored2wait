c65f8847498295e317a8ca8866a69957bf456f3e
MrGeorgen <gf2wb6f7t6tfg67wbf8ebgft6ef@protonmail.com>
2021-05-06 20:31:55 +0200

    format code

--- check.js
+++ check.js
@@ -1,6 +1,5 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
-const { exec} = require('child_process');
 const boxen = require('boxen');
 var pjson = require('./package.json');
 var cv1 = pjson.version;
@@ -9,37 +8,30 @@ var cv = 'v' + cv1;
 
 (async () => {
 
-    let feed = await parser.parseURL('https://github.com/themoonisacheese/2bored2wait/releases.atom');
+	let feed = await parser.parseURL('https://github.com/themoonisacheese/2bored2wait/releases.atom');
+
+	feed.items.every(item => {
+		console.log(item.title);
+		var lv = (item.title);
+		if (cv != lv) {
+			console.log(boxen('New Update Available!', {padding: 1}));
+			console.log('Press enter to continue.');
+			process.stdin.once('data', function () {
+				console.log("Starting 2b2w");
+				require('./main.js');
+
+			});
+
+		} else {
+			console.log("Starting 2b2w");
+			require('./main.js');
+
+		}
 
 
 
-    feed.items.every(item => {
-        console.log(item.title);
-        var lv = (item.title);
-        if (cv != lv) {
-	    console.log(boxen('New Update Available!', {padding: 1}));
-            console.log('Press enter to continue.');
-	    process.stdin.once('data', function () {
-  	    console.log("Starting 2b2w");
-	    require('./main.js');
-             
-		});
-            
-        } else {
-		console.log("Starting 2b2w");
-		require('./main.js');
+	});
 
-		
-
-	
-	
-		
-	}
-
-
-        
-    });
-    
 
 })();
 
