7735ac2845f8b1e88047dc624b56d671f4277750
themoonisacheese <oliviersenn6@gmail.com>
2021-05-07 16:01:21 +0200

    add particles.js files in exe build

--- build.js
+++ build.js
@@ -14,7 +14,7 @@ build("macos-x64");
 function build(platform) {
 	compile({
 		input: "cli.js",
-		resources: ["webserver/index*", "nexe-conf", "node_modules/discord.js/src"],
+		resources: ["webserver/index*", "nexe-conf", "node_modules/discord.js/src", "node_modules/particles.js/particles.js", "node_modules/particles.js/demo/js/app.js"],
 		name: `build/2bored2wait-${platform}-${pack.version}`,
 		target: `${platform}-14.15.3`
 	});
