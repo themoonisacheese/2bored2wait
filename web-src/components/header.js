aaad459c9581a5fbaa67b89164e6fbc21d67e6f1
nothotscott <idontwantyoutospam@gmail.com>
2021-08-11 23:02:50 -0500

    Mobile viewport fix

--- web-src/components/header.js
+++ web-src/components/header.js
@@ -23,7 +23,7 @@ import Logo from '../../images/logo.png'
 export default class Header extends Component {
 	render() {
 		return (<header>
-			<Navbar bg="dark" variant="dark" className="mb-lg-4">
+			<Navbar bg="dark" variant="dark" className="mb-4">
 				<Container>
 					<Navbar.Brand href="index.html">
 						<img src={Logo} width="30" height="30" className="d-inline-block align-top" /> 2Bored2Wait
