ebdf126b13fdabd0a7b946cb8e95b3975d744393
KozmikNano <spearingnathanial@gmail.com>
2022-06-03 20:59:23 -0400

    Change Check to look at tags (Made by rob), fixed package.json to prevent errors, and patch relase.yml to fix taggin issues

--- check.js
+++ check.js
@@ -15,9 +15,12 @@ var cv1 = pjson.version;
 var cv = 'v' + cv1;
 
 try {
-    config = require("config");
+    config = require("config/parser");
+    config.mergeDeep(config, DEFAULT)
+
 } catch (err) {
     if (String(err).includes("SyntaxError: ")) {
+        console.error("The syntax in your config file is not correct. Make sure you replaced all values as the README says under 'How to Install' step 5. If it still does not work, check that all quotes are closed. You can look up the json syntax online. Please note that the comments are no problem although comments are normally not allowed in json. " + err)
         process.exit(1);
     }
 }
@@ -108,53 +111,66 @@ function getconf() {
 }
 
 
+function start() {
+    console.log("Please wait...");
+    rl.close();
+    require('./main.js');
+}
+
+if (config.updatemessage === false || config.updatemessage == "n") {
+    start();
+    process.exit(0);
+}
+
 function check() {
-    var updatemessage = config.updatemessage;
-    (async () => {
-        let feed = await parser.parseURL('https://github.com/themoonisacheese/2bored2wait/releases.atom');
-        feed.items.every(item => {
-            var lv = (item.title);
-            if (!cv.includes(lv) && updatemessage != "n") {
-                console.log(boxen('New Update Available! → ' + lv, {
-                    padding: 1,
-                    margin: 1,
-                    align: 'center',
-                    borderColor: 'red',
-                    float: 'center',
-                    borderStyle: 'round'
-                }));
-                rl.question("To continue type 1. To edit settings type 2. ", function (choice) {
-                    if (choice == 1) {
-                        start();
-                    } else if (choice == 2) {
-                        settings();
-                    } else {
-                        console.log("Invalid response.");
-                        check();
-                    };
-                });
-            } else {
+(async () => {
+    let latest = await require("node-fetch")('https://api.github.com/repos/themoonisacheese/2bored2wait/releases/latest');
+
+    let { tag_name, url } = JSON.parse(await latest.text());
+
+    let isLatest = require("./package.json").version.includes(tag_name);
+
+    if (isLatest) {
+        start()
+    } else {
+        let newUpdateMessage = boxen('New Update Available! → ' + tag_name, {
+            padding: 1,
+            margin: 1,
+            align: 'center',
+            borderColor: 'red',
+            float: 'center',
+            borderStyle: 'round'
+        });
+        console.log(newUpdateMessage)
+        rl.question("To continue type 1. To edit settings type 2. ", function (choice) {
+            if (choice == 1) {
                 start();
+            } else if (choice == 2) {
+                settings();
+            } else {
+                console.log("Invalid response.");
+                check();
             };
         });
-    })();
-
-    function start() {
-        console.log("Please wait...");
-        rl.close();
-        require('./main.js');
     }
+})()
 
-    function settings() {
-        console.log("Clearing Settings");
-        fs.unlink('config/local.json', (err) => {
-            if (err) {
-                console.log("No settings file.");
-            }
+function start() {
+    console.log("Please wait...");
+    rl.close();
+    require('./main.js');
+}
 
-            console.log("Done.");
+function settings() {
+    console.log("Clearing Settings");
+    fs.unlink('config/local.json', (err) => {
+        if (err) {
+            console.log("No settings file.");
+        }
 
-        });
-        start();
-    }
+        console.log("Done. Run 2bored2wait again!");
+
+    });
+    start();
+}
 }
