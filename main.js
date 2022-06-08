c7f0263e6c918526a79cd4708a801a2beae12902
nickcat325 <80810636+nickcat325@users.noreply.github.com>
2022-06-08 13:27:35 -0500

    New commands
    Adds page 2 of help.
    Adds command named "Stats" that displays your health and hunger.

--- main.js
+++ main.js
@@ -408,7 +408,9 @@ function userInput(cmd, DiscordOrigin, discordMsg, channel) {
 
 	switch (cmd) {
 		case "help":
+		case "help 1":
 		case "commands":
+			console.log(" Help 1 of 2.\n");
 			console.log(" help: Lists available commands.");
 			console.log(" start 14:00: Start queue at 2pm.");
 			console.log(" play 8:00: Tries to calculate the right time to join so you can play at 8:00am.");
@@ -419,6 +421,29 @@ function userInput(cmd, DiscordOrigin, discordMsg, channel) {
 			console.log(" url: displays the github url");
 			console.log(" stop: Stops the queue.");
 			console.log(" exit or quit: Exits the application.");
+			break;
+		case "help 2":
+		case "commands 2":
+			console.log(" Help 2 of 2.\n");
+			console.log(" stats: Displays your health and hunger.");
+			break;
+
+		case "stats":
+			try {
+			if (conn.bot.health == undefined && conn.bot.food == undefined){
+			console.log("Unknown.")
+			break;}
+			else
+			{if (conn.bot.health == 0)
+			console.log("Health: DEAD");
+			else
+			console.log("Health: " + Math.ceil(conn.bot.health)/2 + "/10");
+			if (conn.bot.food == 0)
+			console.log("Hunger: STARVING");
+			else
+			console.log("Hunger: " + conn.bot.food/2 + "/10");}
+			} catch (err)
+			{console.log(`Start 2B2W first with "Start".`)}
 			break;
 
 		case "url":
